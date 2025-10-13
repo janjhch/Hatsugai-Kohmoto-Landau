4c2169264a8b0a82bd7e7900cb762220266c596d
Jan Heinrich <heinrich@users.itp.uni-frankfurt.de>
2025-10-13 18:44:01 +0200

    backup

--- HKLnumerics/HKLseparable_3D.py
+++ HKLnumerics/HKLseparable_3D.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import root
+from scipy.optimize import root, fixed_point
 
 t = 1
 d = 3
@@ -25,25 +25,33 @@ def I_3(x: float, y: float):
     shifted_I_1 = x + 2 * t * y * (np.cos(k1k1) + np.cos(k2k2))
     func_val_grid = I_1(shifted_I_1, y)
 
-    intermediate = np.trapezoid(func_val_grid, k2_array, axis=1)
-    result = np.trapezoid(intermediate, k1_array)
+    intermediate = np.trapz(func_val_grid, k2_array, axis=1)
+    result = np.trapz(intermediate, k1_array)
 
     return result / ((2 * np.pi)**2)
 
-
+"""
 # np.where(if statement, if true, if false)
 def J_1(x: np.ndarray, y: float):
     return np.where(np.abs(x) < (2 * t * y),
              - 1 / (np.pi * np.sqrt(np.square(2 * t * y) - np.square(x))),
              0)
+"""
+
+def J_1(x: np.ndarray, y: float):
+    inside = np.abs(x) < (2 * t * y)
+    result = np.zeros_like(x, dtype=float)
+    result[inside] = -1 / (np.pi * np.sqrt((2 * t * y)**2 - x[inside]**2))
+    return result
+
 
 
 def J_3(x: float, y: float):
     shifted_I_1 = x + 2 * t * y * (np.cos(k1k1) + np.cos(k2k2))
     func_val_grid = J_1(shifted_I_1, y)
 
-    intermediate = np.trapezoid(func_val_grid, k2_array, axis=1)
-    result = np.trapezoid(intermediate, k1_array)
+    intermediate = np.trapz(func_val_grid, k2_array, axis=1)
+    result = np.trapz(intermediate, k1_array)
 
     return d * result / ((2 * np.pi)**2)
 
@@ -51,6 +59,11 @@ def J_3(x: float, y: float):
 def GLS_3d(rho: float, mu: float, U: float, e_tilde: float, f_0: float, f_1: float):
     eq1 = rho - (I_3(mu - f_0 * rho, 1 + f_1 * e_tilde) + I_3(mu - U - f_0 * rho, 1 + f_1 * e_tilde))
     eq2 = e_tilde - (J_3(mu - f_0 * rho, 1 + f_1 * e_tilde) + J_3(mu - U - f_0 * rho, 1 + f_1 * e_tilde))
+    return [eq1, eq2]
+
+def GLS_3d_fixed_point(rho: float, mu: float, U: float, e_tilde: float, f_0: float, f_1: float):
+    eq1 = I_3(mu - f_0 * rho, 1 + f_1 * e_tilde) + I_3(mu - U - f_0 * rho, 1 + f_1 * e_tilde)
+    eq2 = J_3(mu - f_0 * rho, 1 + f_1 * e_tilde) + J_3(mu - U - f_0 * rho, 1 + f_1 * e_tilde)
     return [eq1, eq2]
 
 # Do not solve SOE for mu, but for rho
@@ -65,11 +78,11 @@ def solve_GLS_3d_for_rho(mu: float, U: float, f_0: float, f_1: float):
     else:
         # Model as two different lines for each band
         if mu <= 2 * t * d:
-            guess_rho = mu / (4 * t) + 1 / (2 * t)
+            guess_rho = mu / (4 * t * d) + 1 / 2
         elif 2 * t * d < mu <= U - 2 * t * d:
             guess_rho = 1
         elif U - 2 * t * d < mu:
-            guess_rho = mu / (4 * t) + (3 - U / (2 * t)) / 2
+            guess_rho = mu / (4 * t * d) + 1 - (U - 2 * t * d) / (4 * t * d)
 
     if U <= 4 * t * d:
         a = 1
@@ -78,39 +91,86 @@ def solve_GLS_3d_for_rho(mu: float, U: float, f_0: float, f_1: float):
         # Model as ax**2 + bx + c
         guess_e = a * mu**2 + b * mu + c
     else:
-        a = 0.5
+        a = 1 / 36
         # Model as two different parabolas for each band
-        if mu <= 2 * t:
-            guess_e = a * (mu**2 - 4 * t**2 * a)
-        elif 2 * t < mu <= U - 2 * t:
+        if mu <= 2 * t * d:
+            guess_e = a * (mu**2 - 4 * (t * d)**2)
+        elif 2 * t * d < mu <= U - 2 * t * d:
             guess_e = 0
-        elif U - 2 * t < mu:
-            b = - a *  (4 * t * U) / (U + 2 * t)
-            c = - (U**2 + 2 * t**2) * a - U * b
+        elif U - 2 * t * d < mu:
+            b = - a / 2
+            c = - a * (U**2 - U/2 + 3*t*d - 4*(t*d)**2)
             guess_e = a * mu**2 + b * mu + c
 
     guess = [guess_rho, guess_e]
 
-    sol = root(GLS_reduced, guess, method='hybr', tol=1e-3)
+    sol = root(GLS_reduced, guess, method='broyden1', tol=1e-3)
     # Should return list [rho, e_tilde] for any given mu
     return sol.x
 
+def solve_GLS_3d_for_rho_fixed_point(mu: float, U: float, f_0: float, f_1: float):
+    # def x = [rho, e_tilde]
+    GLS_reduced = lambda x: GLS_3d_fixed_point(x[0], mu, U, x[1], f_0, f_1)
+
+    # Guess something near a possible solution
+    if U <= 4 * t * d:
+        # Model as one straight line through start and endpoint
+        guess_rho = (2 / (4 * t + U)) * mu + 4 * t / (4 * t + U)
+    else:
+        # Model as two different lines for each band
+        if mu <= 2 * t * d:
+            guess_rho = mu / (4 * t * d) + 1 / 2
+        elif 2 * t * d < mu <= U - 2 * t * d:
+            guess_rho = 1
+        elif U - 2 * t * d < mu:
+            guess_rho = mu / (4 * t * d) + 1 - (U - 2 * t * d) / (4 * t * d)
+
+    if U <= 4 * t * d:
+        a = 1
+        b = - a * (4 * t + U**2) / (4 * t + U)
+        c = 2 * t * b - 4 * t**2 * a
+        # Model as ax**2 + bx + c
+        guess_e = a * mu**2 + b * mu + c
+    else:
+        a = 1 / 72
+        # Model as two different parabolas for each band
+        if mu <= 2 * t * d:
+            guess_e = a * (mu**2 - 4 * (t * d)**2)
+        elif 2 * t * d < mu <= U - 2 * t * d:
+            guess_e = 0
+        elif U - 2 * t * d < mu:
+            b = - a / 2
+            c = - a * (U**2 - U/2 + 3*t*d - 4*(t*d)**2)
+            guess_e = a * mu**2 + b * mu + c
+
+    sol = fixed_point(GLS_reduced, [guess_rho, guess_e], xtol=1e-3)
+
+    return sol
 
 # Given an array of mu values, calculate correponding rho and e_tilde values, which are returned as separate arrays
 # Uses direct 2d root finding
-def create_solution_arrays_rho_e_root(mu_array: np.ndarray, U: float, f_0: float, f_1: float):
+def create_solution_arrays_rho_e_root(mu_array: np.ndarray, U: float, f_0: float, f_1: float, use_fixed_point=False):
     rho_list = []
     e_tilde_list = []
     N = len(mu_array)
     i = 0
 
-    for mu_val in mu_array:
+    if use_fixed_point == True:
+        for mu_val in mu_array:
+            print(f'\rProgress: {(i / N * 100):.1f}%{' ' * 20}', end="", flush=True)
+            sol = solve_GLS_3d_for_rho(mu_val, U, f_0, f_1)
+            rho_list.append(sol[0])
+            e_tilde_list.append(sol[1])
+            i += 1
         print(f'\rProgress: {(i / N * 100):.1f}%{' ' * 20}', end="", flush=True)
-        sol = solve_GLS_3d_for_rho(mu_val, U, f_0, f_1)
-        rho_list.append(sol[0])
-        e_tilde_list.append(sol[1])
-        i += 1
-    print(f'\rProgress: {(i / N * 100):.1f}%{' ' * 20}', end="", flush=True)
+    else:
+        for mu_val in mu_array:
+            print(f'\rProgress: {(i / N * 100):.1f}%{' ' * 20}', end="", flush=True)
+            sol = solve_GLS_3d_for_rho_fixed_point(mu_val, U, f_0, f_1)
+            rho_list.append(sol[0])
+            e_tilde_list.append(sol[1])
+            i += 1
+        print(f'\rProgress: {(i / N * 100):.1f}%{' ' * 20}', end="", flush=True)
 
     rho_array = np.array(rho_list)
     e_tilde_array = np.array(e_tilde_list)
